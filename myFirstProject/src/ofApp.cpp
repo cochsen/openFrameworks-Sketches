8595cdd52a3bf83c6d3927d2ecee8019f09b0cfc
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2015-12-30 14:19:02 -0500

    create shapes, animate, set framerate

--- myFirstProject/src/ofApp.cpp
+++ myFirstProject/src/ofApp.cpp
@@ -1,18 +1,34 @@
 #include "ofApp.h"
 
+int myCircleX;									// declare variables
+int myCircleY;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetFrameRate(60);							// Set framerate
 
+	myCircleX = 300;							// initialize variables
+	myCircleY = 200;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	myCircleX += 4;								// increment x coordinate
+	if (myCircleX > 1024)						// check bounds of display
+	{
+		myCircleX = 300;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofSetColor(255, 0, 255);					// set color
+	ofDrawCircle(myCircleX, myCircleY, 60);		// draw circle at (x,y), radius 60
 
+	ofSetColor(0, 255, 255);
+	ofDrawCircle(500, 500, 100);
+
+	ofDrawBitmapString(ofToString(ofGetFrameRate()) + " fps", 10, 15);	// show framerate
 }
 
 //--------------------------------------------------------------
