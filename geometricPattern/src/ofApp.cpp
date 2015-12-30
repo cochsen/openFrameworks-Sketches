5168a30f93125eb5de35e67c67a0999d192c56cd
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2015-12-30 17:56:07 -0500

    add function declaration to header and function to program

--- geometricPattern/src/ofApp.cpp
+++ geometricPattern/src/ofApp.cpp
@@ -1,14 +1,8 @@
 #include "ofApp.h"
 
-int myCircleX;
-int myCircleY;
-int myCircleRadius;
-
 //--------------------------------------------------------------
 void ofApp::setup(){
-	myCircleX = 300;
-	myCircleY = 200;
-	myCircleRadius = 60;
+
 }
 
 //--------------------------------------------------------------
@@ -18,59 +12,50 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofSetColor(255, 0, 255);
-	ofDrawCircle(myCircleX, myCircleY, myCircleRadius);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::stripePattern() {
+	ofSetColor(ofColor::black);
+	ofSetLineWidth(3.0);
+	for (int i = -50; i < 50; i++)
+	{
+		ofPushMatrix();
+		ofTranslate(i * 20, 0);
+		ofLine(0, -100, 0, 100);
+		ofPopMatrix();
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-	cout << "keyPressed " << (char)key << endl;
-	if (key == 'w')	// key 119
-	{
-		myCircleY -= 4;
-	}
-	if (key == 's')	// key 115
-	{
-		myCircleY += 4;
-	}
-	if (key == 'a')	// key 97
-	{
-		myCircleX -= 4;
-	}
-	if (key == 'd')	// key 100
-	{
-		myCircleX += 4;
-	}
+
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-	cout << "keyReleased " << key << endl;
+
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-	cout << "mouseMoved: " << x << ", " << y << endl;
+
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-	cout << "mouseDragged: " << x << ", " << y << " button: " << button << endl;
+
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-	cout << "mousePressed: " << x << ", " << y << " button: " << button << endl;
-	float distance = ofDist(myCircleX, myCircleY, x, y);
-	if (distance < myCircleRadius)
-	{
-		myCircleRadius += 2;
-	}
+
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-	cout << "mouseReleased: " << x << ", " << y << " button: " << button << endl;
+
 }
 
 //--------------------------------------------------------------
