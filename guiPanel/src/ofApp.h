995e620afb09b279eb5b9ff8b887b99c4da7e53c
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-01-01 15:33:52 -0500

    added features
    - matrixPattern() drawing function
    - show/hide GUI
    - take screenshot
    - save/load setting presets

--- guiPanel/src/ofApp.h
+++ guiPanel/src/ofApp.h
@@ -4,40 +4,45 @@
 #include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-		void stripePattern();
-		void exit();
-
-		ofxPanel gui;
-		ofxIntSlider countX;
-		ofxFloatSlider stepX;
-		ofxFloatSlider twistX;
-		
-		ofxGuiGroup globalGroup;
-		ofxFloatSlider Scale;
-		ofxFloatSlider Rotate;
-		ofxFloatSlider Background;
-
-		ofxGuiGroup primGroup;
-		ofxFloatSlider shiftY, rotate;
-		ofxVec2Slider size;
-		ofxColorSlider color;
-		ofxToggle filled, type;
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void stripePattern();
+    
+    ofxPanel gui;
+    ofxIntSlider countX;
+    ofxFloatSlider stepX;
+    ofxFloatSlider twistX;
+    
+    void exit();
+    
+    ofxGuiGroup globalGroup;
+    ofxFloatSlider Scale;
+    ofxFloatSlider Rotate;
+    ofxFloatSlider Background;
+    
+    ofxGuiGroup primGroup;
+    ofxFloatSlider shiftY, rotate;
+    ofxVec2Slider size;
+    ofxColorSlider color;
+    ofxToggle filled, type;
+    
+    bool showGui;
+    
+    ofxIntSlider countY;
+    ofxFloatSlider stepY, twistY, pinchY;
+    void matrixPattern();
+  
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
 };
